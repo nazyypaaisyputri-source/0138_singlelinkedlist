22fa8375dc287a235a7db06b9999388dd42e41a2
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-23 11:57:50 +0700

    implementasi fungsi traverse

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -100,5 +100,17 @@ void addNode()
             {
                 cout << "\nList Kosong\n";
             }
+            else
+        {
+            cout << "\nData di dalam list adalah:\n";
+            Node *currentNode = START;
+            
+            while (currentNode != NULL)
+            {
+                  cout << currentNode->noMhs << endl;
+                  currentNode = currentNode->next;
+            }
+            cout << endl;
         }
+    }
 };
