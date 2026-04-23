b4231b5d46fdcf240acebb123f35c678d3086f9b
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-23 11:24:27 +0700

    looping mencari posisi insert yang sesuai

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -43,5 +43,10 @@ void addNode()
 
         Node *previous = START;
         Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            
+        }
     }
 };
