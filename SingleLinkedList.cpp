ac861ab4934b5892d8338b694193f69c2b6cfc6e
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-23 11:14:07 +0700

    menambahkan validasi duplikat NIM diawal list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -20,12 +20,21 @@ public:
     }
 
 void addNode() 
-{
+    {
         int nim;
         cout << "\nMasukkan Nomor Mahasiswa : ";
         cin >> nim;
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
-}
+
+        if (START == NULL || nim <= START->noMhs) 
+        {
+            if (START != NULL && nim == START->noMhs) 
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+    }
 };
